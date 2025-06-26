0cad1cf82c035a2f0f5f416cda714b033b1800f8
Brian Seong <brianseong.dev@gmail.com>
2025-06-26 03:15:01 -0700

    feat: updating background ui to make it less clutter

--- shared/components/AggniverseBackground.tsx
+++ shared/components/AggniverseBackground.tsx
@@ -11,12 +11,18 @@ export default function AggniverseBackground() {
       {/* Particles above cosmic background */}
       <div className="absolute inset-0">
         <Particles
-          particleColors={['#ffffff', '#ffffff']}
-          particleCount={200}
+          particleColors={[
+            '#ffcc6f', // Red/Orange (M-type)
+            '#ffd2a1', // Orange (K-type)
+            '#fff4ea', // Yellow/White (G-type)
+            '#ffffff', // White (A/F-type)
+            '#aabfff', // Blue (O/B-type)
+          ]}
+          particleCount={1000}
           particleSpread={10}
           speed={0.03}
-          particleBaseSize={100}
-          moveParticlesOnHover={true}
+          particleBaseSize={30}
+          moveParticlesOnHover={false}
           alphaParticles={false}
           disableRotation={false}
         />
--- shared/components/CosmicBackground.tsx
+++ shared/components/CosmicBackground.tsx
@@ -2,8 +2,8 @@
 export default function CosmicBackground() {
   return (
     <>
-      <div className="o o1" />
-      <div className="o o2" />
+      {/* <div className="o o1" /> */}
+      {/* <div className="o o2" /> */}
       <div className="o o3" />
       <div className="o o4" />
     </>
